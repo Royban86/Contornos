b93d0062061944fb1f5164236c8661d68610db6b
R <R@R-PC01>
2024-04-17 13:41:15 +0200

    Commit uno

---
+++ src/main/java/cuentas/cuenta/Cuenta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cuentas.cuenta;
+
+/**
+ *
+ * @author R
+ */
+public class Cuenta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
